389996a7f63682e364197f9ae1953542d2f23930
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-02-26 14:28:47 +0100

    Création de la grille

---
+++ src/model/Grille.java
@@ -0,0 +1,17 @@
+
+package model;
+
+
+public class Grille {
+    
+    /**
+     * Modélisation de la grille par un tableau à deux dimensions.
+     * String vide = Rien
+     * "0" = Case vide attaqué
+     * "B" = Présence d'un bateau
+     * "X" = Une partie du bateau touché
+     */
+    private final String[][] grille = new String[10][10];
+    
+    
+}
